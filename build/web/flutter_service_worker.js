e3519be7a24ebc007cf62319be86fce8082ded2c
iService571 <axmad5498@gmail.com>
2026-02-17 17:40:36 +0500

    build web change commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c796092aa6da7c885eee22117f0879e4
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5c5d03063399a9eb4af1c765433ca301",
+"flutter_bootstrap.js": "1ea03255d47b249a284d13c8f7909391",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
