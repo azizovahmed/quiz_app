dce4d1e187a998a946e6e2bc2c62361619bf42e0
iService571 <axmad5498@gmail.com>
2026-02-24 17:43:33 +0500

    flutter build web commit 3

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -107,159 +107,161 @@ class _QuizPageState extends State<QuizPage> {
       ),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Jarayon",
-                        style: TextStyle(
-                          color: Colors.grey.shade700,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                        ),
-                      ),
-                      Text(
-                        "$expendedValue/10",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w900,
-                          fontSize: 19,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Container(
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: Colors.red.shade100,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Expanded(
-                          flex: expendedValue,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
+                        Text(
+                          "Jarayon",
+                          style: TextStyle(
+                            color: Colors.grey.shade700,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
                           ),
                         ),
-                        Expanded(child: Container(), flex: 10 - expendedValue),
+                        Text(
+                          "$expendedValue/10",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 19,
+                          ),
+                        ),
                       ],
                     ),
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    widget.tests[expendedValue - 1].quizanswer,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 30),
-                  SelectedItemTest(
-                    variant: widget.tests[expendedValue - 1].v1,
-                    onTap: () {
-                      setState(() {
-                        selectedVariant = "A";
-                      });
-                    },
-                    isSelected: selectedVariant == "A",
-                  ),
-                  SelectedItemTest(
-                    variant: widget.tests[expendedValue - 1].v2,
-                    onTap: () {
-                      setState(() {
-                        selectedVariant = "B";
-                      });
-                    },
-                    isSelected: selectedVariant == "B",
-                  ),
-                  SelectedItemTest(
-                    variant: widget.tests[expendedValue - 1].v3,
-                    onTap: () {
-                      setState(() {
-                        selectedVariant = "C";
-                      });
-                    },
-                    isSelected: selectedVariant == "C",
-                  ),
-                  SelectedItemTest(
-                    variant: widget.tests[expendedValue - 1].v4,
-                    onTap: () {
-                      setState(() {
-                        selectedVariant = "D";
-                      });
-                    },
-                    isSelected: selectedVariant == "D",
-                  ),
-                ],
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                setState(() {
-                  DateTime endTime = DateTime.now();
-
-                  Duration difference = endTime.difference(startTime);
-
-                  int seconds = difference.inSeconds;
-                  if (selectedVariant == "") return;
-                  if(selectedVariant==widget.tests[expendedValue - 1].trueAnswer){
-                    trueAnswerCount++;
-                    trueorfalse.add(true);
-                  }else{
-                    trueorfalse.add(false);
-                  }
-                  if (currentQuistionIndex < 9) {
-
-                    expendedValue++;
-                    selectedVariant = "";
-                    currentQuistionIndex++;
-                    _countDownController.restart();
-                  } else {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) {
-                          return ResultPage(quizs: widget.tests, trueAnswerCount: trueAnswerCount, ttrueorfalse: trueorfalse, timeSpent: seconds,);
-                        },
+                    SizedBox(height: 15),
+                    Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.red.shade100,
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                    );
-                  }
-                });
-              },
-              child: Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(30),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: expendedValue,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          Expanded(child: Container(), flex: 10 - expendedValue),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      widget.tests[expendedValue - 1].quizanswer,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 30),
+                    SelectedItemTest(
+                      variant: widget.tests[expendedValue - 1].v1,
+                      onTap: () {
+                        setState(() {
+                          selectedVariant = "A";
+                        });
+                      },
+                      isSelected: selectedVariant == "A",
+                    ),
+                    SelectedItemTest(
+                      variant: widget.tests[expendedValue - 1].v2,
+                      onTap: () {
+                        setState(() {
+                          selectedVariant = "B";
+                        });
+                      },
+                      isSelected: selectedVariant == "B",
+                    ),
+                    SelectedItemTest(
+                      variant: widget.tests[expendedValue - 1].v3,
+                      onTap: () {
+                        setState(() {
+                          selectedVariant = "C";
+                        });
+                      },
+                      isSelected: selectedVariant == "C",
+                    ),
+                    SelectedItemTest(
+                      variant: widget.tests[expendedValue - 1].v4,
+                      onTap: () {
+                        setState(() {
+                          selectedVariant = "D";
+                        });
+                      },
+                      isSelected: selectedVariant == "D",
+                    ),
+                  ],
                 ),
-                child: Center(
-                  child: Text(
-                    "Keyingi ▶️",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    DateTime endTime = DateTime.now();
+          
+                    Duration difference = endTime.difference(startTime);
+          
+                    int seconds = difference.inSeconds;
+                    if (selectedVariant == "") return;
+                    if(selectedVariant==widget.tests[expendedValue - 1].trueAnswer){
+                      trueAnswerCount++;
+                      trueorfalse.add(true);
+                    }else{
+                      trueorfalse.add(false);
+                    }
+                    if (currentQuistionIndex < 9) {
+          
+                      expendedValue++;
+                      selectedVariant = "";
+                      currentQuistionIndex++;
+                      _countDownController.restart();
+                    } else {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) {
+                            return ResultPage(quizs: widget.tests, trueAnswerCount: trueAnswerCount, ttrueorfalse: trueorfalse, timeSpent: seconds,);
+                          },
+                        ),
+                      );
+                    }
+                  });
+                },
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Keyingi ▶️",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 60),
-          ],
+              SizedBox(height: 60),
+            ],
+          ),
         ),
       ),
     );
